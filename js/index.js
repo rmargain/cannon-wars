3dbf4f47c7136d8313cc9aad2c38ff3d9a20da20
Roberto Margain <margain.roberto@gmail.com>
2020-10-19 23:39:22 -0500

    second cannon

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
 canvas.width = 1200 //window.innerWidth;
 canvas.height = 600 //window.innerHeight;
+let activePlayer = 2;
 
 // 1. Instancia de fondo
 let img = new Image();
@@ -14,7 +15,7 @@ let backgroundImage = {
 }
 }
 
-// Cannon
+// Cannon Left
 let cannonImage1 = new Image();
 let cannonBase1 = new Image();
 let cannonWheel1 = new Image();
@@ -59,6 +60,50 @@ let cannon1 = {
     },
 }
 
+// Cannon Right
+let cannonImage2 = new Image();
+let cannonBase2 = new Image();
+let cannonWheel2 = new Image();
+cannonImage2.src = './images/facing-left/body-red.png';
+cannonBase2.src = './images/facing-left/body-bordered.png';
+cannonWheel2.src = './images/facing-left/wheel-bordered.png';
+let cannon2 = {
+    img: cannonImage2,
+    base: cannonBase2,
+    wheel: cannonWheel2,
+    launchAngle: 0,
+    x: canvas.width - 50,
+    speed: 2,
+
+    //
+    aimUp: function() {
+      this.launchAngle += 1;
+  },
+    aimDown: function() {
+        this.launchAngle -= 1;
+  },
+    moveRight: function() {
+      this.x += this.speed;
+  },
+    moveLeft: function () {
+        this.x -= this.speed;
+    },
+
+  //
+    draw: function () {
+        ctx.save();
+        ctx.translate(this.x - 30, canvas.height-160+24);
+        ctx.rotate(this.launchAngle*.017);
+        ctx.drawImage(this.img, -30, -(24), 63, 48); //check negativs
+        ctx.restore();
+        ctx.drawImage(this.base, this.x - 50, canvas.height-140, 80, 38);
+        ctx.save();
+        ctx.translate(this.x - 8 - 12, canvas.height-120+12);
+        ctx.rotate(this.x*6 * 0.017);
+        ctx.drawImage(this.wheel,-12, -12, 24, 24); // check negatives
+        ctx.restore();
+    },
+}
 
 //Botón de iniciar juego
 window.onload = function() {
@@ -69,12 +114,21 @@ window.onload = function() {
 
 //moviemientos
 document.addEventListener('keydown', e => {
-  switch(e.keyCode){
+  if(activePlayer === 1){
+    switch(e.keyCode){
     case 37: cannon1.moveLeft(); break;
     case 39: cannon1.moveRight(); break;
     case 38: cannon1.aimUp(); break;
     case 40: cannon1.aimDown(); break;
   }
+} else if (activePlayer === 2){
+    switch(e.keyCode){
+    case 37: cannon2.moveLeft(); break;
+    case 39: cannon2.moveRight(); break;
+    case 38: cannon2.aimUp(); break;
+    case 40: cannon2.aimDown(); break;
+  }
+}
   updateCanvas();
 });
 
@@ -102,7 +156,11 @@ const updateCanvas = () => {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   backgroundImage.draw();
-  cannon1.draw();
+  
+        cannon1.draw();
+  
+        cannon2.draw();
+  
 //   cannon1.aimDown();
 //   cannon1.aimUp();
 //   cannon1.rotateWheel();
